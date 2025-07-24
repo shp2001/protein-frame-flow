a0f34725a90836590f66069cb757b54077d090ef
Park Sanghyun <cwp2001@snu.ac.kr>
2025-07-24 16:57:58 +0900

    .

--- experiments/train_se3_flows.sh
+++ experiments/train_se3_flows.sh
@@ -10,4 +10,4 @@
 
 
 echo "Running train_abb3.py"
-torchrun --nproc_per_node=1794110 /home/psh/protein-frame-flow/experiments/train_se3_flows.py > /home/psh/protein-frame-flow/experiments/logs/CDRFlow_v1.4.0.log
+torchrun --nproc_per_node=2 /home/psh/protein-frame-flow/experiments/train_se3_flows.py > /home/psh/protein-frame-flow/experiments/logs/CDRFlow_v1.4.0.log
