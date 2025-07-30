1d36c222a233627411ca9fa27657c3c21ce82c20
Park Sanghyun <cwp2001@snu.ac.kr>
2025-07-30 22:36:17 +0900

    ref_pos debug

--- data/featurizer.py
+++ data/featurizer.py
@@ -99,7 +99,6 @@ def get_ref_basic_feature(aatype_batch, atom_14_mask_batch, res_indices_batch):
                                                                             # ]
 
         res_idx = res_indices[i]
-        res_coords = []
         for j, atom_name in enumerate(atom_names):
             if atom_name != '':
                 ref_space_uid.append(res_idx)
@@ -123,15 +122,12 @@ def get_ref_basic_feature(aatype_batch, atom_14_mask_batch, res_indices_batch):
                 for atom in atom_coords:
                     if atom[0] == atom_name:
                         coord = atom[-1]  # 좌표 (x, y, z)
-                        res_coords.append(coord)
                         break
                     
                 if coord is None:
                     raise ValueError(f"atom_name '{atom_name}' not found in atom_coords.")
 
-        res_coords = np.array(res_coords)
-        transformed_res_coords = random_transform(res_coords)
-        ref_pos.extend(transformed_res_coords.tolist())
+                ref_pos.append(coord)
     
     ref_atom_name_chars = atom_name_chars_encoded(atom_list)
     ref_space_uid = torch.tensor(ref_space_uid).unsqueeze(0).repeat(B,1)
