45eca1b706fcede75eb72a66f69837485b356f4d
Park Sanghyun <cwp2001@snu.ac.kr>
2025-08-20 23:06:56 +0900

    .

--- data/protein_dataloader.py
+++ data/protein_dataloader.py
@@ -53,7 +53,7 @@ class ProteinData(LightningDataModule):
 
                     cropped_feat[key] = ["".join(chain_seq) for chain_seq in cropped_seq_list]
 
-            relpos_emb = embed_relpos(feat['residue_index'], cropped_feat['chain_seq_list'])
+            relpos_emb = embed_relpos(cropped_feat['residue_index'], cropped_feat['chain_seq_list'])
             cropped_feat['pair_init'] = relpos_emb
             cropped_feat['csv_idx'] = feat['csv_idx']
             cropped_feat['crop_idx'] = torch.tensor(feat['crop_idx'])
