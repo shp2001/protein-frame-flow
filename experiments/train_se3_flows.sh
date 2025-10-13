c18fecee81a63646e85c78acafa0e6d47a930cb4
Park Sanghyun <cwp2001@snu.ac.kr>
2025-10-13 23:37:47 +0900

    .

--- experiments/train_se3_flows.sh
+++ experiments/train_se3_flows.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-#SBATCH -J v2.3.3_general
+#SBATCH -J v2.3.2.1_general
 #SBATCH -p gpu
 #SBATCH --gres=gpu:A6000:4
 #SBATCH --mem=200g
 #SBATCH -c 64
-#SBATCH -w gpu05
+#SBATCH -w gpu04
 #SBATCH -o /home/psh/protein-frame-flow/experiments/logs/train_fm.log
 #SBATCH -e /home/psh/protein-frame-flow/experiments/logs/train_fm.err
 
@@ -12,4 +12,4 @@
 echo "Running train_abb3.py"
 echo $CUDA_VISIBLE_DEVICES  # 여러 개 나와야 함
 torchrun --standalone --nnodes=1 --nproc_per_node=4 /home/psh/protein-frame-flow/experiments/train_se3_flows.py \
-    > /home/psh/protein-frame-flow/experiments/logs/v2.3.3_general.log 
+    > /home/psh/protein-frame-flow/experiments/logs/v2.3.2.1_general.log 
